b01c938c223b2cd718285f1d312c0c1f759bcf3c
benjaminpeere <benjamin.peere@viacesi.fr>
2019-05-07 15:22:12 +0200

    servlet url

--- src/com/test/servlet/testServlet.java
+++ src/com/test/servlet/testServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "testServlet", urlPatterns = "/test")
+@WebServlet(name = "testServlet", urlPatterns = "/jenkins/job/toto/test")
 public class testServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
