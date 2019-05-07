9b9d1e075ea26223777288667a897f12a0f24ef1
benjaminpeere <benjamin.peere@viacesi.fr>
2019-05-07 14:25:14 +0200

    test

---
+++ src/com/test/servlet/testServlet.java
@@ -0,0 +1,20 @@
+package com.test.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "testServlet", urlPatterns = "/test")
+public class testServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = "Transmission de données à la vue";
+        request.setAttribute("test", message);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
+    }
+
+}
--- web/WEB-INF/test.jsp
+++ web/WEB-INF/test.jsp
@@ -2,15 +2,20 @@
   Created by IntelliJ IDEA.
   User: Utilisateur
   Date: 18/04/2019
-  Time: 11:28
+  Time: 11:55
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-  <title>$Title$</title>
+    <title>Test</title>
 </head>
 <body>
-$END$
+<p>Page généré avec jsp</p>
+
+<%
+    String attribut = (String) request.getAttribute("test");
+    out.println(attribut);
+%>
 </body>
 </html>
